1eb82f3c5c7d7877cce7a2edb5ba0a864cb7ca46
Pedro Lucas <pedro_leoti@hotmail.com>
2020-02-29 23:40:44 -0300

    Complementação da tela de login (faz a requisição para API), adição do Lottie para animação de carregamento

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -1,11 +1,32 @@
-import React from "react";
-import { View, Text, TouchableOpacity, TextInput } from "react-native";
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  TextInput,
+  AsyncStorage
+} from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
-
+import api from "../../api";
+import LottieView from "lottie-react-native";
 import { Container, Input, ViewInput, Botao, Texto } from "./styles";
 
 export default function Login({ navigation }) {
-  return (
+  const [user, setUser] = useState({ email: "", password: "" });
+  const [ready, setReady] = useState(true);
+  async function login() {
+    setReady(false);
+    try {
+      const { email, password } = user;
+      const response = await api.post("/login", { email, password });
+      console.log(response.data);
+    } catch (e) {
+      console.log(e);
+    }
+    setReady(true);
+  }
+
+  return ready ? (
     <Container>
       <ViewInput>
         <MaterialIcons
@@ -17,16 +38,23 @@ export default function Login({ navigation }) {
           placeholder="Email"
           keyboardType="email-address"
           autoCapitalize="none"
+          value={user.email}
+          onChangeText={d => setUser({ ...user, email: d })}
         />
       </ViewInput>
 
       <ViewInput>
         <MaterialIcons name="lock" size={25} style={{ position: "absolute" }} />
 
-        <Input placeholder="Senha" secureTextEntry={true} />
+        <Input
+          placeholder="Senha"
+          secureTextEntry={true}
+          value={user.password}
+          onChangeText={d => setUser({ ...user, password: d })}
+        />
       </ViewInput>
 
-      <Botao onPress={() => ""}>
+      <Botao onPress={() => login()}>
         <Texto>Entrar</Texto>
       </Botao>
 
@@ -34,5 +62,11 @@ export default function Login({ navigation }) {
         <Texto>Cadastro</Texto>
       </TouchableOpacity>
     </Container>
+  ) : (
+    <LottieView
+      source={require("../../../assets/loading.json")}
+      autoPlay
+      loop
+    />
   );
 }
---
+++ src/interfaces/user.ts
@@ -0,0 +1,6 @@
+export default interface User {
+  nome: string;
+  email: string;
+  tel: string;
+  senha: string;
+}
