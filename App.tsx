078ecaf734969bae976932ed187b5ba678a7c860
Plucks77 <pedro_leoti@hotmail.com>
2020-05-19 19:03:59 -0300

    Substituição do Drawer pelo bottomtab

--- App.tsx
+++ App.tsx
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from "react";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { FontAwesome } from "@expo/vector-icons";
+import { FontAwesome5 } from "@expo/vector-icons";
 
 import Login from "./src/components/Login";
 import Cadastro from "./src/components/Cadastro";
@@ -9,11 +12,15 @@ import Locais from "./src/components/Locais";
 import Cidades from "./src/components/Cidades";
 import Pacotes from "./src/components/Pacotes";
 import Pacote from "./src/components/Pacote";
+import Perfil from "./src/components/Perfil";
+import Configuracoes from "./src/components/Configuracoes";
 import { AsyncStorage } from "react-native";
 import LottieView from "lottie-react-native";
+import Globals from "./src/styles/global";
 
 const Drawer = createDrawerNavigator();
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   const [ready, setReady] = useState(false);
@@ -43,20 +50,17 @@ export default function App() {
 
   return ready ? (
     <NavigationContainer>
-      <Drawer.Navigator
-        initialRouteName={user_logado === false ? "Login" : "Principal"}
+      <Stack.Navigator
+        headerMode="none"
+        initialRouteName={user_logado ? "Main" : "Login"}
       >
-        <Drawer.Screen
-          name="Principal"
-          component={PrincipalStack}
-          options={{ drawerLabel: "Selecionar cidade" }}
+        <Stack.Screen name="Login" component={LoginStack} />
+        <Stack.Screen
+          name="Main"
+          component={BottomTab}
+          options={{ gestureEnabled: false }}
         />
-        <Drawer.Screen
-          name="Login"
-          component={LoginStack}
-          options={{ gestureEnabled: false, drawerLabel: "Sair" }}
-        />
-      </Drawer.Navigator>
+      </Stack.Navigator>
     </NavigationContainer>
   ) : (
     <LottieView source={require("./assets/loading.json")} autoPlay loop />
@@ -80,5 +84,50 @@ function LoginStack() {
       <Stack.Screen name="Login" component={Login} />
       <Stack.Screen name="Cadastro" component={Cadastro} />
     </Stack.Navigator>
+  );
+}
+
+function BottomTab() {
+  return (
+    <Tab.Navigator
+      tabBarOptions={{
+        activeTintColor: "black",
+        inactiveTintColor: Globals.tab_menu_secundary,
+        activeBackgroundColor: Globals.tab_menu,
+        inactiveBackgroundColor: Globals.tab_menu,
+        style: {
+          borderTopWidth: 0,
+          borderTopColor: "black",
+        },
+      }}
+    >
+      <Tab.Screen
+        name="Viagens"
+        component={PrincipalStack}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome5 name="suitcase-rolling" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Perfil"
+        component={Perfil}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome name="user" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Configurações"
+        component={Configuracoes}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome name="cog" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
   );
 }
