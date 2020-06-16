94511fb62bb935cb423dd262578d8f370a98922d
Plucks77 <pedro_leoti@hotmail.com>
2020-06-16 15:50:57 -0300

    Adição da cor buttonLight nas variáveis de cores

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -3,6 +3,7 @@ export default {
   text: "#36453B",
   error: "crimson",
   button: "#818BE7",
+  buttonLight: "rgba(129,139,231,0.3)",
   button_text: "#EEEEEE",
   background_text: "#e7eed1",
   tab_menu: "#818BE7",
