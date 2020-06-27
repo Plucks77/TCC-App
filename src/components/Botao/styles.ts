a702aab2fe8d5abc36f5c6810fbd97cf2b14b1ac
Plucks77 <pedro_leoti@hotmail.com>
2020-06-27 15:24:05 -0300

    Mudança da cor do componente botão

--- src/components/Botao/styles.ts
+++ src/components/Botao/styles.ts
@@ -1,16 +1,16 @@
 import styled from "styled-components/native";
-import global from "../../styles/global";
+import global, { palette } from "../../styles/global";
 
 export const BotaoCorpo = styled.TouchableOpacity`
   width: 300px;
   height: 35px;
   border-radius: 10px;
-  background-color: ${global.button};
+  background-color: ${palette.primary};
   align-items: center;
   justify-content: center;
 `;
 
 export const BotaoTexto = styled.Text`
-  color: ${global.button_text};
+  color: ${palette.white};
   font-weight: bold;
 `;
