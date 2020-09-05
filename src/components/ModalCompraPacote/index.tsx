49c4099e527f58fc55ee4b3ab3ad33d5094e6367
Plucks77 <pedro_leoti@hotmail.com>
2020-09-05 12:32:56 -0300

    Estilização da modal de pós compra melhorada e separada em outro arquivo

--- src/components/ModalCompraPacote/index.tsx
+++ src/components/ModalCompraPacote/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Text, Modal, View, TouchableOpacity } from "react-native";
 
-import { Container } from "./styles";
+import { Container, Titulo, Texto, Botao, BotaoTexto, ViewExterna, ViewInterna } from "./styles";
 
 interface props {
   visible: boolean;
@@ -12,38 +12,15 @@ const ModalCompraPacote: React.FC<props> = ({ visible, setShowModal }) => {
   return (
     <Container>
       <Modal animationType="slide" transparent={true} visible={visible}>
-        <View
-          style={{
-            flex: 1,
-            justifyContent: "center",
-            alignItems: "center",
-            marginTop: 22,
-          }}
-        >
-          <View
-            style={{
-              margin: 20,
-              backgroundColor: "white",
-              borderRadius: 20,
-              padding: 35,
-              alignItems: "center",
-              shadowColor: "#000",
-              shadowOffset: {
-                width: 0,
-                height: 2,
-              },
-              shadowOpacity: 0.25,
-              shadowRadius: 3.84,
-              elevation: 5,
-            }}
-          >
-            <Text>Sucesso!</Text>
-            <Text>Você pode ver seu pacote comprado no novo menu que surgiu ali embaixo! </Text>
-            <TouchableOpacity onPress={() => setShowModal(false)}>
-              <Text>Fechar</Text>
-            </TouchableOpacity>
-          </View>
-        </View>
+        <ViewExterna>
+          <ViewInterna>
+            <Titulo>Sucesso!</Titulo>
+            <Texto>Você pode ver seu pacote comprado no novo menu que surgiu ali embaixo! </Texto>
+            <Botao onPress={() => setShowModal(false)}>
+              <BotaoTexto>Fechar</BotaoTexto>
+            </Botao>
+          </ViewInterna>
+        </ViewExterna>
       </Modal>
     </Container>
   );
--- src/components/ModalCompraPacote/styles.ts
+++ src/components/ModalCompraPacote/styles.ts
@@ -5,3 +5,41 @@ export const Container = styled.View`
   flex: 1;
   background-color: ${palette.white};
 `;
+
+export const Titulo = styled.Text`
+  font-size: 16px;
+  margin-bottom: 10px;
+`;
+
+export const Texto = styled.Text``;
+
+export const Botao = styled.TouchableOpacity`
+  margin: 20px 0px;
+`;
+
+export const BotaoTexto = styled.Text`
+  color: blue;
+`;
+
+export const ViewExterna = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  margin-top: 22px;
+`;
+
+export const ViewInterna = styled.View`
+  margin: 20px;
+  background-color: white;
+  border-radius: 20px;
+  padding: 35px;
+  padding-bottom: 0px;
+  align-items: center;
+  shadow-offset: {
+    width: 0;
+    height: 2;
+  }
+  shadow-opacity: 0.25;
+  shadow-radius: 3.84px;
+  elevation: 5;
+`;
